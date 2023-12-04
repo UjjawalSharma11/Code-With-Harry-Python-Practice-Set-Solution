2fd7c1b04d183be064aefd75ffabf3f31824ec9b
UjjawalSharma11 <149385700+UjjawalSharma11@users.noreply.github.com>
2023-12-04 19:23:19 +0530

    Add files via upload

---
+++ Practice Sets.py
@@ -0,0 +1,122 @@
+# Practice Set1:
+"""
+#Q1).
+print("Twinkle Twinkle Little Star")
+
+#Q2).
+Num = int(input("Num: "))
+for i in range(1, 11):
+    Table = Num * i
+    print(Num, "X", i, "=", Table)
+
+
+#Practice Set2:
+
+#Q1).
+a = int(input("num1: "))
+b = int(input("num2: "))
+print("Sum is", a + b)
+
+
+#Q2).
+a = int(input("num1: "))
+print("Remainder when", a, "is divided by 2 is:", a % 2)
+
+
+#Q3).
+X = input("Var: ")
+print("Class of", X, "is:", type(X))
+
+
+#Q4).
+a = int(input("num1: "))
+b = int(input("num2: "))
+
+if a > b:
+    print(True)
+else:
+    print(False)
+
+
+#Q5).
+a = int(input("num1: "))
+b = int(input("num2: "))
+print("Average of", a, "and", b, "is:", (a+b)/2)
+
+
+#Q6).
+a = int(input("num1: "))
+print("Square of", a, "is:", a**2)
+"""
+
+#Practice Set3:
+"""
+#Q1).
+Name = input("Name: ")
+print("Good Morniing", Name)
+
+
+#Q2). {Doubt}
+
+#Q3).
+a = input("a: ")
+if "  " in a:
+    print("Double Space Detected")
+else:
+    print("No Double Space")
+
+
+#Q4).
+
+#Q5).
+print("Dear Harry,\nThis Python course is good.\n\t Thanks")
+"""
+
+#Practice Set4:
+"""
+#Q)1.
+Data = input("Data: ")
+list = Data.split(",")
+print(list)
+
+#Q2).
+Data = input("Data: ")
+Data1 = Data.split()
+list = list(map(int, Data1))
+print("Marks of 6 students:", list)
+
+
+#Q3). Tuples are immutable
+Data = input("Data: ")
+T1 = tuple(Data)
+T1.append(2)
+print(T1)
+"""
+"""
+#Q4).
+Data = input("Data: ")
+Data1 = Data.split(",")
+L1 = list(map(int, Data1))
+print(L1)
+
+sum = 0
+for i in L1:
+    sum = sum + i
+print("Sum is", sum)
+
+
+#Q5).
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
+"""
+
+#Practice Set5:
+
+#Q1).
+
+
+
+
+
+
+
